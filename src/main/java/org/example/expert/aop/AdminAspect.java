8f868067c922c15fc23eaa5db760a7fa6f4f63c9
MinjuKang <minjukang727@gmail.com>
2024-09-11 18:55:50 +0900

    필수 기능 Level.2  9. AOP - (패키지 기반) 특정 api 접근 로그 기록하기 (수정)
    AOP 를 이용하여 어드민 사용자만 접근할 수 있는 특정 API 에 접근할 때마다 접근 로그 기록하기
    
    **주의사항!**
    
    - **로그 기록에는 다음 정보가 포함되어야 합니다**
        - 요청한 사용자의 ID
        - API 요청 시각
        - API 요청 URL
    
    주의 사항을 빠뜨려서
    [{API 요청 시간}] Access User : {요청한 사용자 ID}, RequestURL : {API 요청 URL}
    형식으로 로그가 남도록 코드 수정하였습니다.

--- src/main/java/org/example/expert/aop/AdminAspect.java
+++ src/main/java/org/example/expert/aop/AdminAspect.java
@@ -1,9 +1,14 @@
 package org.example.expert.aop;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.example.expert.domain.common.annotation.Auth;
+import org.example.expert.domain.common.dto.AuthUser;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
@@ -20,12 +25,15 @@ public class AdminAspect {
     @Around("adminDeleteComment() || adminChangeUserRole()")
     public void execute(ProceedingJoinPoint joinPoint) throws Throwable {
         // 접근 로그
-        log.trace("[{}] {} 접근",new Date(), joinPoint.getStaticPart());
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        StringBuffer requestURL = request.getRequestURL();
+        Long userId = (Long) request.getAttribute("userId");
+        log.info("[{}] Access User : {} , RequestURL : {}",new Date(), userId, requestURL);
 
         try {
             joinPoint.proceed();
         } finally {
-            log.trace("{} : {} 접근 해제", new Date(), joinPoint.getStaticPart());
+            log.info("[{}] 사용자 접근 해제",new Date());
         }
     }
 }
